1b2f10210aa1685cebbdb78ea84d04eecef6bc44
Maahir Bhavsar <maahirbhavsar@gmail.com>
2026-07-20 10:13:45 +0530

    Built-in function

---
+++ built in function/Statement length, letter occurrence .py
@@ -0,0 +1,5 @@
+s="Heyram said: Quality is show our silent personality"
+print(s)
+print("Length of statement=", len(s))
+print("The first occurrence of the letter a=", s.index('a'))
+print("t occurs", s.count('t'), "times.")
