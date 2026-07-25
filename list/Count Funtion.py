9c0eef1e4358cc9664274dbf981307e90bcec81f
Maahir Bhavsar <maahirbhavsar@gmail.com>
2026-07-25 09:46:24 +0530

    added list programs

---
+++ list/Count Funtion.py
@@ -0,0 +1,5 @@
+list=[123, 'xyz', 'Heyram', 'abc', 123]
+print("Count for 123:", list.count(123))
+
+# count function shows how many times element is there in a list
+# it is shown by variable.count(element)
