d9f7fee221bbaef9b97732391b854cb13653d5b7
Maahir Bhavsar <maahirbhavsar@gmail.com>
2026-08-10 08:43:50 +0530

    Added instruction for the code

--- FIle input and output/Function 1.py
+++ FIle input and output/Function 1.py
@@ -1,11 +1,18 @@
 import os
 
+os.getcwd()
+print(os.getcwd())
+print("Show directory")
 
-os.getcwd();
-print("Show directory");
-
-os.rmdir("Heyram/abcd");
+os.rmdir("Heyram/abcd")
 print("Directory Delete")
 
 os.listdir("Heyram")
 print("List Directory")
+
+
+# Here I have imported os first.
+# os.getcwd means that it is asking os to bring current working directory
+# now os.rmdir means remove "EMPTY" directory. Please note the directory should be entirely empty for this to work
+# os.listdir() is a python function from os module.
+# it will return a list of all the files and directories in the giver path
