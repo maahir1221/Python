ab7257b0733ad92953efa7d88178ad7bec52a9be
Maahir Bhavsar <maahirbhavsar@gmail.com>
2026-08-14 09:05:17 +0530

    read me for 7. Decorator eg2.py

--- Generator Iterator Decorator/7. Decorator eg2.py
+++ Generator Iterator Decorator/7. Decorator eg2.py
@@ -3,6 +3,11 @@ def make_pretty(fun):
         print("I got decorated")
         fun()
     return inner()
-@make_pretty
+# @make_pretty
 def ordinary():
     print("I am ordinary")
+
+print(make_pretty(ordinary))
+
+# You can either use @function to call it or call it normally
+# @FUNCTION IS COMMNETED above, if you want to use it than comment line 10 or else there will be an error
