5781cb90316ce23ed8e5e17bb0867625a6f4b8f6
Maahir Bhavsar <maahirbhavsar@gmail.com>
2026-08-18 08:50:21 +0530

    Some practice file added (OOPS)

--- 10. OOPS/1. Basic OOPS Program.py
+++ 10. OOPS/1. Basic OOPS Program.py
@@ -1,8 +1,33 @@
-class Number:
-    def sum(self):
-        return self.a+self.b
-no=Number()
-no.a=2
-no.b=10
-ans=no.sum()
-print("Sum =", ans)
+# class Number:
+#     def sum(self):
+#         return self.a+self.b
+# no=Number()
+# no.a=2
+# no.b=10
+# ans=no.sum()
+# print("Sum =", ans)
+
+# class ab:
+#     # def __init__(self):
+#     #     print("hello")
+#     def __init__(self,a,b):
+#         print(a,b)
+# b=ab(6,7)
+
+class ab:
+    def __init__(self):
+        self.a=6
+        self.b=7
+    def show(self):
+        print(self.a," ",self.b)
+
+class bc:
+    print("hello")
+    def getdata(self):
+        print("meshwa")
+b=ab()
+b.show()
+
+m=bc()
+m.getdata()
+
--- Everyday Practice/OOPS.py
+++ Everyday Practice/OOPS.py
@@ -2,11 +2,17 @@
 #     pass
 # mycar=car()
 
-class car:
-    def __init__(self, brand, color):
-        self.brand=brand
-        self.color=color
+# class car:
+#     def __init__(self, brand, color):
+#         self.brand=brand
+#         self.color=color
+#
+# mycar=car("BMW", "Emrald Green")
+# print(mycar.brand)
+# print(mycar.color)
 
-mycar=car("BMW", "Emrald Green")
-print(mycar.brand)
-print(mycar.color)
+class sum:
+    def __init__(self, a, b):
+        self.sum = a + b
+        print(self.sum)
+x=sum(19, 11)
