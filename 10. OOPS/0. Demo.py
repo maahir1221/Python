5298dfeef99ea748433a1da3f8492ab501588ed1
Maahir Bhavsar <maahirbhavsar@gmail.com>
2026-08-21 15:46:23 +0530

    added new oops demo

--- 10. OOPS/0. Demo.py
+++ 10. OOPS/0. Demo.py
@@ -1,9 +1,25 @@
-class getdata:
+# class getdata:
+#     def show(self):
+#         print("hello")
+#     def show1(self):
+#         print("welcome")
+#     print("hello")
+# obj=getdata()
+#
+# obj.show1()
+
+
+class a:
     def show(self):
         print("hello")
+class b:
     def show1(self):
         print("welcome")
-    print("hello")
-obj=getdata()
 
-obj.show1()
+class c(a,b):
+    def show2(self):
+        print("meshwa")
+o=c()
+o.show()
+o.show2()
+# o.show1()
